8a4c0182a72577bf1c3a277a4f0160278f8fb5dd
Валерия Майкова <vamaylik@gmail.com>
2024-04-03 16:12:40 +0300

    Добавила новые автотестики.

--- Auto2/UnitTest1.cs
+++ Auto2/UnitTest1.cs
@@ -11,6 +11,10 @@ namespace Auto2
         public void TestMethod1()
         {
             Assert.AreEqual(DemoExam.Exam("15", "10", "5"), 30);
+            Assert.AreEqual(DemoExam.Exam("22", "38", "20"), 80);
+            Assert.AreEqual(DemoExam.Exam("1", "1", "1"), 3);
+            Assert.AreEqual(DemoExam.Exam("0", "0", "0"), 0);
+            Assert.AreEqual(DemoExam.Exam("21", "37", "19"), 77);
             //Assert.AreNotEqual(DemoExam.Exam("0", "10", "5"), 30);
             //Assert.AreNotEqual(DemoExam.Exam("t", "0", "-1"), 30);
         }
